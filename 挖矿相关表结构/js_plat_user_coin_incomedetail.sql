758865106e3d5207c6e8719fd6bc46fe9fe9765e
liaochj <1072163919@qq.com>
2019-07-18 23:17:19 +0800

    表结构更新，增加汇总表。

--- 挖矿相关表结构/js_plat_user_coin_incomedetail.sql
+++ 挖矿相关表结构/js_plat_user_coin_incomedetail.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50725
 File Encoding         : 65001
 
-Date: 2019-07-06 19:39:06
+Date: 2019-07-18 23:15:27
 */
 
 SET FOREIGN_KEY_CHECKS=0;
--- 挖矿相关表结构/js_plat_user_income_incomedetail.sql
+++ 挖矿相关表结构/js_plat_user_income_incomedetail.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50725
 File Encoding         : 65001
 
-Date: 2019-07-06 19:39:20
+Date: 2019-07-18 23:15:38
 */
 
 SET FOREIGN_KEY_CHECKS=0;
