b283be4450591790b3c22161b6e7931cb7c1e683
liaochj <1072163919@qq.com>
2019-07-27 01:26:17 +0800

    流通总量不显示问题处理。

--- biao/biao-mysql/src/main/java/com/biao/sql/build/CoinSqlBuild.java
+++ biao/biao-mysql/src/main/java/com/biao/sql/build/CoinSqlBuild.java
@@ -5,6 +5,6 @@ import com.biao.sql.BaseSqlBuild;
 
 public class CoinSqlBuild extends BaseSqlBuild<Coin, Integer> {
 
-    public static final String columns = "id,parent_id,name,full_name,domain,whitepaper_url,status,token_status,token_volume,withdraw_max_volume,withdraw_min_volume,withdraw_day_max_volume,withdraw_fee_type,withdraw_fee,ico_price,remarks,coin_type";
+    public static final String columns = "id,parent_id,name,full_name,domain,whitepaper_url,status,token_status,token_volume,withdraw_max_volume,withdraw_min_volume,withdraw_day_max_volume,withdraw_fee_type,withdraw_fee,ico_price,remarks,coin_type,circulate_volume";
 
 }
