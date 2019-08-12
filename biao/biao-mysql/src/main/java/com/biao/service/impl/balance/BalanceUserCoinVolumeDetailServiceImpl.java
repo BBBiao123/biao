385b61eb36888fab839fd40231175c39caef5c9d
liaochj <1072163919@qq.com>
2019-08-12 20:12:55 +0800

    收益加上事务控制。

--- biao/biao-mysql/src/main/java/com/biao/service/impl/balance/BalanceUserCoinVolumeDetailServiceImpl.java
+++ biao/biao-mysql/src/main/java/com/biao/service/impl/balance/BalanceUserCoinVolumeDetailServiceImpl.java
@@ -22,6 +22,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -101,6 +102,7 @@ public class BalanceUserCoinVolumeDetailServiceImpl implements BalanceUserCoinVo
     }
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public void  balanceIncomeCount(){
         List<BalanceUserCoinCountVolume>  balanceUserCoinVolumeList= balanceUserCoinCountVolumeDao.findAll();
         if (CollectionUtils.isNotEmpty(balanceUserCoinVolumeList)) {
